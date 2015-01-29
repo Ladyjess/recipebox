8d31ce2cb01e226bbea86332e46b4835747456ff
Andy Uppendahl & Jessica Hori <student@epicodus.com>
2015-01-29 10:39:06 -0800

    load main pages

--- app.rb
+++ app.rb
@@ -9,3 +9,17 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 get('/') do
   erb(:index)
 end
+
+get("/recipes") do
+  @recipes = Recipe.all()
+  erb(:recipe)
+end
+
+post("/recipes") do
+  recipe_name = params.fetch("recipe_name")
+  recipe_desc = params.fetch("recipe_desc")
+  recipe = Recipe.create({ :name => recipe_name,
+    :description => recipe_desc })
+  @recipes = Recipe.all()
+  erb(:recipe)
+end
